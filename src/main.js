589452acb7a44b4878a555e8d07fa5eee8460502
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-03-26 12:56:41 +0300

    Hello world

---
+++ src/main.js
@@ -0,0 +1,7 @@
+function firstFunction(name) {
+    if(name === String) {
+        console.log('Hello, world! My name is ' + name);
+    } else {
+        console.log('Hello, world! I would to stay incognito');
+    }
+}
