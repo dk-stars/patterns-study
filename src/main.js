ace30beb3357e39049217f72dc327032fe4fdad4
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-03-26 13:21:10 +0300

    Fix lol

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 function firstFunction(name) {
-    if(name === String) {
+    if(typeof name === 'string') {
         console.log('Hello, world! My name is ' + name);
     } else {
         console.log('Hello, world! I would to stay incognito');
